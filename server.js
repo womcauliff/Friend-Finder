9eddc8640ba82b053ae50e2c28c61c27b3595a53
Will McAuliff <womcauliff@gmail.com>
2016-09-23 13:53:15 -0400

    Added Procfile for Heroku deployment. Adjusted port selection.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +12,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
-app.listen(PORT, function () {
-	console.log('App listening on PORT ' + PORT);
+app.listen(app.get('port'), function () {
+	console.log('App listening on PORT ' + app.get('port'));
 });
