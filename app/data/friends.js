abce97fd97048c05b3899500fd398438e856df76
Will McAuliff <womcauliff@gmail.com>
2016-09-25 01:19:25 -0400

    Backend determines most combatible match for user's scores, sends JSON profile

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,23 @@
 var friends = [
     {
+        "name":"Ahmed",
+        "image":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
         "name": "Will",
-        "photo": "http://will.mcauliff.org/images/profile_pic.png",
+        "image": "http://will.mcauliff.org/images/profile_pic.png",
         "scores": [
             5,
             5,
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -8,12 +8,51 @@ module.exports = function(app) {
 
 	app.post('/api/friends', function (req, res) {
 		var newFriend = req.body;
+
 		console.log(
 			'POSTED' +
 			"\nname: " + newFriend.name +
 			"\nimage: " + newFriend.image +
 			"\nscores: " + newFriend.scores
 		);
-		res.json(true);
+
+		var bestMatch = friendData[combatibilityCheck(newFriend.scores)];
+		console.log(
+			'BESTMATCH' +
+			"\nname: " + bestMatch.name +
+			"\nimage: " + bestMatch.image +
+			"\nscores: " + bestMatch.scores
+		);
+		res.json(bestMatch);//send compatible match JSON
 	});
+
+	function combatibilityCheck(userScores) {
+
+		var bestMatchDiff = 50;//initialize best match diff at worst possible value
+		var bestMatchIndex = 0;//intialize best match index as first profile index
+
+		//loop through existing profiles
+		for (var i = 0; i < friendData.length; i++) {
+			var friend = friendData[i];
+
+			//loop through existing profile's scores and new user's scores
+			var totalDiff = 0;
+			for (var j = 0; j < friend.scores.length; j++) {
+				var s1 = friend.scores[j];
+				var s2 = userScores[j];
+				totalDiff += Math.abs(s1 - s2);//add diff for individual film to total diff
+			}
+
+			//exit on perfect match
+			if(totalDiff === 0) {
+				return i;
+			}
+
+			if(totalDiff < bestMatchDiff) {
+				bestMatchDiff = totalDiff;//update, new best match found 
+				bestMatchIndex = i;
+			}
+		}
+		return bestMatchIndex;
+	}
 };
